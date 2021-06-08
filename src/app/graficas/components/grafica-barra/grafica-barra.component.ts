5a9d9dda87bdbe9aa4fdd888132afe9e6c1e872f
Pablo Alvarez <pabloalvarezseguel@gmail.com>
2021-06-08 14:44:24 -0400

    seccion 22 clase 333

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica-barra',
@@ -12,5 +14,18 @@ export class GraficaBarraComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public barChartLabels: Label[] = ['2020', '2021', '2022', '2023', '2024', '2025', '2026'];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+
+  public barChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor:'#A255E0', hoverBackgroundColor:'blue' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor:'#EDEB4E', hoverBackgroundColor:'blue' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series C', backgroundColor:'#85ED15', hoverBackgroundColor:'blue' },
+  ];
 
 }
