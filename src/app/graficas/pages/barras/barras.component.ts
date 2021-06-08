ddd957789e63816d71be067ae4938330bb7e8303
Pablo Alvarez <pabloalvarezseguel@gmail.com>
2021-06-08 13:37:16 -0400

    seccion 22 clase 332

--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -12,22 +12,15 @@ export class BarrasComponent implements OnInit {
 
   public barChartOptions: ChartOptions = {
     responsive: true,
-    // We use these empty structures as placeholders for dynamic theming.
-    scales: { xAxes: [{}], yAxes: [{}] },
-    plugins: {
-      datalabels: {
-        anchor: 'end',
-        align: 'end',
-      }
-    }
   };
-  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartLabels: Label[] = ['2020', '2021', '2022', '2023', '2024', '2025', '2026'];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
 
   public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor:'#A255E0', hoverBackgroundColor:'blue' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor:'#EDEB4E', hoverBackgroundColor:'blue' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series C', backgroundColor:'#85ED15', hoverBackgroundColor:'blue' },
   ];
   constructor() { }
 
@@ -35,15 +28,36 @@ export class BarrasComponent implements OnInit {
   }
 
   public randomize(): void {
-    // Only Change 3 values
+   
     this.barChartData[0].data = [
-      Math.round(Math.random() * 100),
-      59,
-      80,
-      (Math.random() * 100),
-      56,
-      (Math.random() * 100),
-      40 ];
+      Math.round(Math.random()*100),
+      Math.round(Math.random()*100),
+      Math.round(Math.random()*100),
+      Math.round(Math.random()*100),
+      Math.round(Math.random()*100),
+      Math.round(Math.random()*100),
+      Math.round(Math.random()*100),
+    ]
+
+    this.barChartData[1].data = [
+      Math.round(Math.random()*100),
+      Math.round(Math.random()*100),
+      Math.round(Math.random()*100),
+      Math.round(Math.random()*100),
+      Math.round(Math.random()*100),
+      Math.round(Math.random()*100),
+      Math.round(Math.random()*100),
+    ]
+
+    this.barChartData[2].data = [
+      Math.round(Math.random()*100),
+      Math.round(Math.random()*100),
+      Math.round(Math.random()*100),
+      Math.round(Math.random()*100),
+      Math.round(Math.random()*100),
+      Math.round(Math.random()*100),
+      Math.round(Math.random()*100),
+    ]
   }
 
 }
