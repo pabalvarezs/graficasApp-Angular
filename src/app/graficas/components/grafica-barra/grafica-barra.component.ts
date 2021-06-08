78bfd67efebdb98a17c78aa7dbfcb2e70bdf96b1
Pablo Alvarez <pabloalvarezseguel@gmail.com>
2021-06-08 15:18:49 -0400

    seccion 22 clase 334

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Label } from 'ng2-charts';
 
@@ -13,19 +13,27 @@ export class GraficaBarraComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if (this.horizontal) {
+      this.barChartType = 'horizontalBar'
+    }
+
   }
+
+  @Input () horizontal : boolean = false;
 
   public barChartOptions: ChartOptions = {
     responsive: true,
   };
-  public barChartLabels: Label[] = ['2020', '2021', '2022', '2023', '2024', '2025', '2026'];
+  @Input() barChartLabels: Label[] = [
+    // '2020', '2021', '2022', '2023', '2024', '2025', '2026'
+  ];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
 
-  public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor:'#A255E0', hoverBackgroundColor:'blue' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor:'#EDEB4E', hoverBackgroundColor:'blue' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series C', backgroundColor:'#85ED15', hoverBackgroundColor:'blue' },
+  @Input() barChartData: ChartDataSets[] = [
+    // { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor:'#A255E0', hoverBackgroundColor:'blue' },
+    // { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor:'#EDEB4E', hoverBackgroundColor:'blue' },
+    // { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series C', backgroundColor:'#85ED15', hoverBackgroundColor:'blue' },
   ];
 
 }
