7a2388ee856b5474abea6ffba3c17a9111bd3c48
Pablo Alvarez <pabloalvarezseguel@gmail.com>
2021-06-09 13:49:48 -0400

    seccion 22 clase 336

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,8 +12,10 @@ import { MenuComponent } from './shared/menu/menu.component';
     MenuComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
