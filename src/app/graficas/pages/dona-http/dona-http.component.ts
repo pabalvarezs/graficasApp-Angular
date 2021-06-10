e402b2f6bf517627347b2298277c435156a89413
Pablo A.S PC Laublo <pabloalvarezseguel@gmail.com>
2021-06-09 20:38:29 -0400

    seccion 22 clase 337

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
 import { GraficasService } from '../../services/graficas.service';
 
 @Component({
@@ -16,10 +18,34 @@ export class DonaHttpComponent implements OnInit {
     this.graficasService.getUsuariosRedesSociales()
       .subscribe( data => {
 
-        console.log(data);
+        // console.log(data);
+
+        const labels = Object.keys(data)
+        const values = Object.values(data)
+        this.doughnutChartLabels = labels;
+        this.doughnutChartData.push(values);
         
       })
   
   }
+
+  public doughnutChartLabels: Label[] = [
+    // 'Download Sales', 'In-Store Sales', 'Mail-Order Sales','Others'
+  ];
+  public doughnutChartData: MultiDataSet = [
+    // [350, 450, 100, 159],
+ 
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+  public colors: Color[]= [
+    { backgroundColor: [
+      '#F0D339',
+      '#F75E16',
+      '#DA1FE0',
+      '#1643F7',
+      '#4AF0B5'
+      ]
+    }
+  ]
 
 }
